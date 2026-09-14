f1379d14e26e601b48574221ac09b791b848e4de
nhan2005mk <dongocnhan345@gmail.com>
2026-09-14 08:27:25 +0700

    feat(slot3): làm việc với git

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import About from "./about";
+
+function App() {
+    return (
+        <div>
+            <h1>Ung dung ReactJS</h1>
+            <About />
+        </div>
+    );
+}
+
+export default App;
